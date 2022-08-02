df3b909bfdb09c70ccbb0af458df9b9e51b36a1b
Mohit Patni <mohitpatni0731@gmail.com>
2022-08-02 21:51:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ for i in range(1):
   from dhooks import Webhook
   import re
   import time
+  import pytest
   import requests
 
-  # This is the API url which is used to fetch the rate of the currency.
   url = "https://api.apilayer.com/fixer/convert?to=INR&from=GBP&amount=1"
   IFTTT_WEBHOOKS_URL = 'https://maker.ifttt.com/trigger/{}/with/key/ikjRKm44HQqrnvBh8j0irFxxihKPyRx6OK6LNTK_jBC'
 
@@ -13,15 +13,16 @@ for i in range(1):
     "apikey": "qlaZvLUc7oREf6dPiJDu3ZsrlChj7pwP"
   }
   
- 
-  response = requests.request("GET", url, headers=headers, data = payload)
-  status_code = response.status_code
-  result = response.text
-  lists = list(result.split())
-  final_result = lists[-2]
-  
-  
+  def get_price():
+    response = requests.request("GET", url, headers=headers, data = payload)
+    status_code = response.status_code
+    result = response.text
+    lists = list(result.split())
+    final_result = lists[-2]
+    return final_result
+    assert final_result == int,"It should be string" 
+
   hook = Webhook("https://discord.com/api/webhooks/1003993992360366080/1cS8w3N2GHzW-2aAwLYZ9tpfcHHhRBiASgTIafnfJ1Q--NbC1Exat7lVVzr4mgvH3l5f")
-  data = "Current pound rate in INR is -> "+final_result
+  data = "Current pound rate in INR is -> "+get_price()
   hook.send(data)
   time.sleep(1)
