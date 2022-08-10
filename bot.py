bacc830d73ed1a7124b25ad92336a79d3fc673e1
Mohit Patni <mohitpatni0731@gmail.com>
2022-08-10 11:50:29 +0530

    Update and rename updated_main.py to bot.py

--- bot.py
+++ bot.py
@@ -24,16 +24,16 @@ import ssl
 import smtplib
 
 
-def getting_current_price():
+def get_current_price():
     """Extracting the value of British Pound in Indian Rupees"""
     currency = "convert 1 GBP to INR"
     url = f"https://google.com/search?q={currency}"
-    request_result = requests.get(f"https://google.com/search?q={currency}")
+    fetch_value = requests.get(f"https://google.com/search?q={currency}")
 
-    soup = bs4.BeautifulSoup(request_result.text, "html.parser")
+    soup = bs4.BeautifulSoup(fetch_value.text, "html.parser")
     texts = soup.find("div", class_="BNeawe iBp4i AP7Wnd").text
     texts_split = texts.split(" ")
-    getting_current_price.current_price = float(texts_split[0])
+    get_current_price.current_price = float(texts_split[0])
     
     
 def discord_bot():
@@ -41,19 +41,19 @@ def discord_bot():
     hook = Webhook("Paste the webhook link url of the Discord channel -- ")
 
     # Defining the upper and lower limit to get the alert when it will be triggered
-    upper_limit = 96.50
-    lower_limit = 96.40
+    upper_limit = float(input("Enter the upper limit you want to set -- "))
+    lower_limit = float(input("Enter the lowwer limit you wawnt to set -- "))
     
     if getting_current_price.current_price >= upper_limit:
-        hook.send(f"ALERT HIGH PRICE: Current price of 1 British Pound is = {str(getting_current_price.current_price)} Indian Rupees")
+        hook.send(f"ALERT HIGH PRICE: Current price of 1 British Pound is = {str(get_current_price.current_price)} Indian Rupees")
         upper_limit += 0.50
 
     elif getting_current_price.current_price <= lower_limit:
-        hook.send(f"ALERT LOW PRICE: Current price of 1 British Pound is = {str(getting_current_price.current_price)} Indian Rupees")
+        hook.send(f"ALERT LOW PRICE: Current price of 1 British Pound is = {str(get_current_price.current_price)} Indian Rupees")
         lower_limit -= 0.50
 
     else:
-        hook.send(f"Current price of 1 British Pound is = {str(getting_current_price.current_price)} Indian Rupees")
+        hook.send(f"Current price of 1 British Pound is = {str(get_current_price.current_price)} Indian Rupees")
             
             
 def send_email():
@@ -63,7 +63,7 @@ def send_email():
     reciever = str(input('Enter email address you want to send mail to -- '))
 
     subject = 'AUTOMATED Pound rate email'
-    body = f'Current price of 1 British Pound is = {str(getting_current_price.current_price)} Indian Rupees'
+    body = f'Current price of 1 British Pound is = {str(get_current_price.current_price)} Indian Rupees'
 
     em = EmailMessage()
     em['From'] = sender
@@ -72,8 +72,8 @@ def send_email():
     em.set_content(body)
 
     context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:   
+    port_number = 465
+    with smtplib.SMTP_SSL('smtp.gmail.com', port_number, context=context) as smtp:   
         smtp.login(sender, password)
         smtp.sendmail(sender, reciever, em.as_string())
 
